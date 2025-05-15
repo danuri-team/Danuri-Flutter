1eec43ac93f0120d2733f5b9d762c96be74a19fd
jonghwan <s23061@gsm.hs.kr>
2025-05-15 19:42:44 +0900

    setting :: 디자인 시스템 세팅

---
+++ lib/core/design_system/color.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DanuriColor{
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color main1 = Color(0xFFF5F5F5);
+  static const Color main2 = Color(0xFFF9F9FB);
+  static const Color main3 = Color(0xFFD5D5D5);
+  static const Color main4 = Color(0xFFB8B8B8);
+  static const Color main5 = Color(0xFF777777);
+  static const Color main6 = Color(0xFFC7E2FE);
+  static const Color main7 = Color(0xFF47A0FF);
+  static const Color main8 = Color(0xFFF84848);
+
+
+  // static const Color home1 = Color(0xFF47A0FF); //홈 | '지금 이용하기'
+  // static const Color home2 = Color(0xFFB8B8B8); //홈 | '공간을 사용할 수 있어요 이용 수칙을 꼭 지켜주세요!'
+  // static const Color home3 = Color(0xFFD5D5D5); //홈 | 이용가능 시간 회색 버튼
+
+  // static const Color signUp1 = Color(0xFFF84848); //회원가입 | 휴대폰 번호 입력칸
+  
+  // static const Color deviceHome1 = Color(0xFF777777); //시설 내 단일 디바이스 홈 이용 | 불가능 버튼
+  
+  // static const Color admin1 = Color(0xFFC7E2FE); //관리자 | 이용 현황 버튼
+  // static const Color admin2 = Color(0xFFF9F9FB);
+  // static const Color admin3 = Color(0xFFF5F5F5);
+}
---
+++ lib/core/design_system/text.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DanuriText {
+  //title
+  static final title1ExtraBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 40.sp,
+  );
+  static final title1Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 40.sp,
+  );
+  static final title2ExtraBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 32.sp,
+  );
+  static final title2Bold = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 32.sp,
+  );
+  static final title3ExtraMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 30.sp,
+  );
+
+  //body
+  static final body1Bold = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 28.sp,
+  );
+  static final body2ExtraBold = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 24.sp,
+  );
+  static final body2Bold = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24.sp,
+  );
+  static final body2ExtraMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 24.sp,
+  );
+  static final body2Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 24.sp,
+  );
+  static final body3Bold = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 22.sp,
+  );
+  static final body4Bold = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20.sp,
+  );
+  static final body4Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20.sp,
+  );
+
+  //caption
+  static final caption1ExtraMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 18.sp,
+  );
+  static final caption1Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 18.sp,
+  );
+  static final caption2ExtraMedium = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.sp,
+  );
+  static final caption2Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+  );
+  static final caption3Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14.sp,
+  );
+  static final caption4Medium = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 12.sp,
+  );
+}
