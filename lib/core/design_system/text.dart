da3d25e59ea15dcbe89ff9dde30c5304c55538a4
jonghwan <s23061@gsm.hs.kr>
2025-05-19 20:47:21 +0900

    add :: 폰트 설정

--- lib/core/design_system/text.dart
+++ lib/core/design_system/text.dart
@@ -6,81 +6,100 @@ class DanuriText {
   static final title1ExtraBold = TextStyle(
     fontWeight: FontWeight.w700,
     fontSize: 40.sp,
+    height: 1.2,
   );
   static final title1Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 40.sp,
+    height: 1.2,
   );
   static final title2ExtraBold = TextStyle(
     fontWeight: FontWeight.w700,
     fontSize: 32.sp,
+    height: 1.2,
   );
   static final title2Bold = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 32.sp,
+    height: 1.2,
   );
   static final title3ExtraMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 30.sp,
+    height: 1.2,
   );
 
   //body
   static final body1Bold = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 28.sp,
+    height: 1.2,
   );
   static final body2ExtraBold = TextStyle(
     fontWeight: FontWeight.w700,
     fontSize: 24.sp,
+    height: 1.2,
   );
   static final body2Bold = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 24.sp,
+    height: 1.2,
   );
   static final body2ExtraMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 24.sp,
+    height: 1.2,
   );
   static final body2Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 24.sp,
+    height: 1.2,
   );
   static final body3Bold = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 22.sp,
+    height: 1.2,
   );
   static final body4Bold = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 20.sp,
+    height: 1.2,
   );
   static final body4Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 20.sp,
+    height: 1.2,
   );
 
   //caption
   static final caption1ExtraMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 18.sp,
+    height: 1.2,
   );
   static final caption1Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 18.sp,
+    height: 1.2,
   );
   static final caption2ExtraMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 16.sp,
+    height: 1.2,
   );
   static final caption2Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 16.sp,
+    height: 1.2,
   );
   static final caption3Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 14.sp,
+    height: 1.2,
   );
   static final caption4Medium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 12.sp,
+    height: 1.2,
   );
 }
