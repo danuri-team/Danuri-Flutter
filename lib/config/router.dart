cc992e8f9c39a01fe44e7137f8626d9407db2bef
jonghwan <s23061@gsm.hs.kr>
2025-06-17 17:19:13 +0900

    :construction: 이미지 다운 및 컴포넌트 정의

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,7 +1,4 @@
-import 'package:danuri_flutter/view/authentication_number_input/screen/authentication_number_input_screen.dart';
-import 'package:danuri_flutter/view/home/screen/home_screen.dart';
-import 'package:danuri_flutter/view/login/screen/login_screen.dart';
-import 'package:danuri_flutter/view/signup/screen/signup_screen.dart';
+import 'package:danuri_flutter/view/image.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,27 +6,17 @@ final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final GoRouter router = GoRouter(
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home',
+  initialLocation: '/image',
   routes: [
+    // GoRoute(
+    //   name: '',
+    //   path: '/',
+    //   builder: (context, state) => const ,
+    // ),
     GoRoute(
-      name: 'home',
-      path: '/home',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      name: 'login',
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      name: 'signup',
-      path: '/signup',
-      builder: (context, state) => const SignUpScreen(),
-    ),
-    GoRoute(
-      name: 'authentication_number_input',
-      path: '/authentication_number_input',
-      builder:(context, state) => const AuthenticationNumberInputScreen(),
+      name: 'image',
+      path: '/image',
+      builder: (context, state) => const ImageScreen(),
     ),
   ],
 );
