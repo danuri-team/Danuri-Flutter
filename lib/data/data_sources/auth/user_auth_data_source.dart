1e121b31c81b21ab7bba943cdfe5f3cfbd79a3a9
jonghwan <s23061@gsm.hs.kr>
2025-06-17 16:51:57 +0900

    :construction: 모델 클래스 명 변경

--- lib/data/data_sources/auth/user_auth_data_source.dart
+++ lib/data/data_sources/auth/user_auth_data_source.dart
@@ -1,6 +1,6 @@
 import 'package:danuri_flutter/data/models/auth/admin_auth/response/tokens_response.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/auth_code_login_request.dart';
-import 'package:danuri_flutter/data/models/auth/user_auth/request/login_request.dart';
+import 'package:danuri_flutter/data/models/auth/user_auth/request/user_login_request.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/sign_up_request.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/response/check_success_response.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/response/user_info_response.dart';
@@ -12,7 +12,7 @@ final String baseUrl = dotenv.env['API_URL']!;
 class UserAuthDataSource {
   final dio = AppDio.getInstance();
 
-  Future<CheckSuccessResponse> login(LoginRequest request) async {
+  Future<CheckSuccessResponse> login(UserLoginRequest request) async {
     final response = await dio.post(
       '$baseUrl/auth/user/phone',
       data: request.toJson(),
