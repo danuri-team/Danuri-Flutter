b9f845ca15b767012286ac24a63c40f4879c002f
jonghwan <s23061@gsm.hs.kr>
2025-06-22 16:41:05 +0900

    feat :: 핸드폰 번호 상태관리 적용

--- lib/view/screen/login_screen.dart
+++ lib/view/screen/login_screen.dart
@@ -1,12 +1,14 @@
 import 'dart:developer';
 
 import 'package:danuri_flutter/core/design_system/color.dart';
+import 'package:danuri_flutter/core/provider/phone_number_provider.dart';
 import 'package:danuri_flutter/data/view_model/login_view_model.dart';
 import 'package:danuri_flutter/view/componenets/button/next_button.dart';
 import 'package:danuri_flutter/view/componenets/custom_top_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -28,55 +30,65 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(60.w, 85.h, 61.w, 58.h),
-          child: Column(
-            children: [
-              CustomTopBar(
-                title: '번호를 입력해주세요',
-                subTitle: '인증을 통해 공간을 이용할 수 있어요',
-                needCallBackButton: true,
-                needHelpMeButton: true,
-              ),
-              SizedBox(height: 131.h),
-              SizedBox(
-                width: 500.w,
-                height: 64.h,
-                child: TextFormField(
-                  controller: _phoneNumberController,
-                  maxLength: 11,
-                  keyboardType: TextInputType.phone,
-                  onTapOutside: (event) =>
-                      FocusManager.instance.primaryFocus?.unfocus(),
-                  decoration: InputDecoration(
-                    hintText: '010-0000-0000',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(
-                        width: 1,
-                        color: DanuriColor.line2,
+    return ChangeNotifierProvider<PhoneNumberProvider>(
+      create: (context) => PhoneNumberProvider(),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(60.w, 85.h, 61.w, 58.h),
+            child: Column(
+              children: [
+                CustomTopBar(
+                  title: '번호를 입력해주세요',
+                  subTitle: '인증을 통해 공간을 이용할 수 있어요',
+                  needCallBackButton: true,
+                  needHelpMeButton: true,
+                ),
+                SizedBox(height: 131.h),
+                SizedBox(
+                  width: 500.w,
+                  height: 64.h,
+                  child: TextFormField(
+                    controller: _phoneNumberController,
+                    maxLength: 11,
+                    keyboardType: TextInputType.phone,
+                    onTapOutside: (event) =>
+                        FocusManager.instance.primaryFocus?.unfocus(),
+                    decoration: InputDecoration(
+                      hintText: '010-0000-0000',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: DanuriColor.line2,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 220.h),
-              NextButton(
-                centerText: '다음',
-                onTap: () {
-                  final String phoneNumber = _phoneNumberController.text;
-                  _viewModel.userLogin(
-                      "${phoneNumber.substring(0, 3)}-${phoneNumber.substring(3, 7)}-${phoneNumber.substring(7, 11)}");
-                  // if(_viewModel.error == false){
-                  //   context.go('/home');
-                  // }else if(_viewModel.error == true){
-                  //   context.go('/sign-up');
-                  // }
-                },
-              ),
-            ],
+                SizedBox(height: 220.h),
+                NextButton(
+                  centerText: '다음',
+                  onTap: () async {
+                    final String phoneNumber =
+                        "${_phoneNumberController.text.substring(0, 3)}-${_phoneNumberController.text.substring(3, 7)}-${_phoneNumberController.text.substring(7, 11)}";
+                    context
+                        .read<PhoneNumberProvider>()
+                        .setPhoneNumber(phoneNumber);
+                    await _viewModel
+                        .userLogin(
+                            "${phoneNumber.substring(0, 3)}-${phoneNumber.substring(3, 7)}-${phoneNumber.substring(7, 11)}")
+                        .then((_) {
+                      if (_viewModel.userInfo!.isSuccess == true) {
+                        context.go('/home');
+                      } else {
+                        context.go('/sign-up');
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
