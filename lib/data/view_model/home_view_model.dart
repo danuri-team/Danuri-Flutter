05d27b4d3679dbbd8610be180b4cffbc3af4d704
jonghwan <s23061@gsm.hs.kr>
2025-06-22 15:52:19 +0900

    feat :: 퇴실 api연결

--- lib/data/view_model/home_view_model.dart
+++ lib/data/view_model/home_view_model.dart
@@ -1,31 +1,18 @@
-import 'package:danuri_flutter/data/data_sources/other/item_rental_data_source.dart';
 import 'package:danuri_flutter/data/data_sources/other/space_data_source.dart';
-import 'package:danuri_flutter/data/models/other/rental_item/request/rental_item_request.dart';
-import 'package:danuri_flutter/data/models/other/space/reqeust/register_used_space_request.dart';
-import 'package:danuri_flutter/data/models/other/space/response/space_usage_status.dart';
+import 'package:dio/dio.dart';
 
 class HomeViewModel {
   final SpaceDataSource _spaceDataSource = SpaceDataSource();
-  final ItemRentalDataSource _itemRentalDataSource = ItemRentalDataSource();
 
-  void registerUsedSpace(String spaceId) async {
-    await _spaceDataSource.registerUsedSpace(
-      RegisterUsedSpaceRequest(spaceId: spaceId),
-    );
-  }
+  bool? _error;
+  bool? get error => _error;
 
-  void rentalItem(String usageId, String itemId, int quantity) async {
-    await _itemRentalDataSource.rentalItem(
-      usageId,
-      RentalItemRequest(
-        itemId: itemId,
-        quantity: quantity,
-      ),
-    );
-  }
-
-  Future<List<SpaceUsageStatus>> test() async{
-    final response = await _spaceDataSource.getSpaceUsageStatus();
-    return response;
+  Future<void> exitRoom() async {
+    try {
+      await _spaceDataSource.exitRoom();
+      _error = false;
+    } on DioException catch (_) {
+      _error = true;
+    }
   }
 }
--- lib/view/home/screen/home_screen.dart
+++ lib/view/home/screen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:danuri_flutter/core/design_system/color.dart';
 import 'package:danuri_flutter/core/design_system/text.dart';
 import 'package:danuri_flutter/data/view_model/home_view_model.dart';
@@ -7,6 +5,7 @@ import 'package:danuri_flutter/view/home/widget/select_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:timer_builder/timer_builder.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,91 +17,92 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-          padding: EdgeInsets.fromLTRB(60.w, 103.h, 61.w, 133.h),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w),
-                    child: Column(
+        padding: EdgeInsets.fromLTRB(60.w, 103.h, 61.w, 133.h),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        '반가워요, 공간을 이용 해볼까요',
+                        style: DanuriText.title2
+                            .copyWith(color: DanuriColor.label5),
+                      ),
+                      SizedBox(height: 6.h),
+                      TimerBuilder.periodic(
+                        const Duration(seconds: 1),
+                        builder: (context) {
+                          DateTime time = DateTime.now();
+                          String period = time.hour < 12 ? '오전' : '오후';
+                          return Text(
+                            '송정다누리 청소년센터 - $period ${time.hour > 13 ? time.hour - 12 : time.hour}시 ${time.minute}분',
+                            style: DanuriText.heading1
+                                .copyWith(color: DanuriColor.label3),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    await _viewModel.exitRoom();
+                  },
+                  child: Container(
+                    width: 138.w,
+                    height: 48.h,
+                    decoration: ShapeDecoration(
+                      color: DanuriColor.line1,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          '반가워요, 공간을 이용 해볼까요',
-                          style: DanuriText.title2
-                              .copyWith(color: DanuriColor.label5),
+                        SvgPicture.asset('assets/icons/leading.svg'),
+                        SizedBox(
+                          width: 6.w,
                         ),
-                        SizedBox(height: 6.h),
-                        TimerBuilder.periodic(
-                          const Duration(seconds: 1),
-                          builder: (context) {
-                            DateTime time = DateTime.now();
-                            String period = time.hour < 12 ? '오전' : '오후';
-                            return Text(
-                              '송정다누리 청소년센터 - $period ${time.hour > 13 ? time.hour - 12 : time.hour}시 ${time.minute}분',
-                              style: DanuriText.heading1
-                                  .copyWith(color: DanuriColor.label3),
-                            );
-                          },
+                        Text(
+                          '퇴실하기',
+                          style: DanuriText.body1Normal
+                              .copyWith(color: DanuriColor.label4),
                         ),
                       ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () async{
-                      await _viewModel.test().then((value) => log('screen value = $value'),);
-                    },
-                    child: Container(
-                      width: 138.w,
-                      height: 48.h,
-                      decoration: ShapeDecoration(
-                        color: DanuriColor.line1,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset('assets/icons/leading.svg'),
-                          SizedBox(
-                            width: 6.w,
-                          ),
-                          Text(
-                            '퇴실하기',
-                            style: DanuriText.body1Normal
-                                .copyWith(color: DanuriColor.label4),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 82.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SelectButton(
-                    svgName: 'space.svg',
-                    text: '공간 사용하기',
-                    onTap: () {
-                      _viewModel.registerUsedSpace('spaceId');
-                    },
-                  ),
-                  SizedBox(width: 70.w),
-                  SelectButton(
-                    svgName: 'item.svg',
-                    text: '물품 대여하기',
-                    onTap: () {
-                      _viewModel.rentalItem('usageId', 'itemId', 0);
-                    },
-                  ),
-                ],
-              )
-            ],
-          )),
+                ),
+              ],
+            ),
+            SizedBox(height: 82.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SelectButton(
+                  svgName: 'space.svg',
+                  text: '공간 사용하기',
+                  onTap: () {
+                    context.push('/register-used-space');
+                  },
+                ),
+                SizedBox(width: 70.w),
+                SelectButton(
+                  svgName: 'item.svg',
+                  text: '물품 대여하기',
+                  onTap: () {
+                    context.push('/item-rental');
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
