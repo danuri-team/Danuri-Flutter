b0d05f35550e4e2a2ab4877a2a03a5874a47aa7f
jonghwan <s23061@gsm.hs.kr>
2025-07-03 21:13:50 +0900

    feat :: 만료시간도 storage에 저장

--- lib/core/storage/token_storage.dart
+++ lib/core/storage/token_storage.dart
@@ -3,59 +3,101 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class TokenStorage {
   static const storage = FlutterSecureStorage();
 
-  Future<void> setAdminAccessToken(String adminAccessToken) async {
+  // Set Admin
+  Future<void> setAdminToken(Map<String, dynamic> adminToken) async {
     await storage.write(
       key: 'adminAccessToken',
-      value: adminAccessToken,
+      value: adminToken['access_token']['token'],
     );
-  }
-
-  Future<void> setAdminRefreshToken(String adminRefreshToken) async {
+    await storage.write(
+      key: 'adminAccessTokenExpiredAt',
+      value: adminToken['access_token']['expired_at'],
+    );
     await storage.write(
       key: 'adminRefreshToken',
-      value: adminRefreshToken,
+      value: adminToken['refresh_token']['token'],
+    );
+    await storage.write(
+      key: 'adminRefreshTokenExpiredAt',
+      value: adminToken['refresh_token']['expired_at'],
     );
   }
 
-  Future<void> setDeviceAccessToken(String deviceAccessToken) async {
+  // Set Device
+  Future<void> setDeviceToken(Map<String, dynamic> deviceToken) async {
     await storage.write(
       key: 'deviceAccessToken',
-      value: deviceAccessToken,
+      value: deviceToken['access_token']['token'],
     );
-  }
-
-  Future<void> setDeviceRefreshToken(String deviceRefreshToken) async {
+    await storage.write(
+      key: 'deviceAccessTokenExpiredAt',
+      value: deviceToken['access_token']['expired_at'],
+    );
     await storage.write(
       key: 'deviceRefreshToken',
-      value: deviceRefreshToken,
+      value: deviceToken['refresh_token']['token'],
+    );
+    await storage.write(
+      key: 'deviceRefreshTokenExpiredAt',
+      value: deviceToken['refresh_token']['expired_at'],
     );
   }
 
-  Future<void> setUserAccessToken(String userAccessToken) async {
+  // Set User
+  Future<void> setUserToken(Map<String, dynamic> userToken) async {
     await storage.write(
       key: 'userAccessToken',
-      value: userAccessToken,
+      value: userToken['access_token']['token'],
     );
-  }
-
-  Future<void> setUserRefreshToken(String userRefreshToken) async {
+    await storage.write(
+      key: 'userAccessTokenExpiredAt',
+      value: userToken['access_token']['expired_at'],
+    );
     await storage.write(
       key: 'userRefreshToken',
-      value: userRefreshToken,
+      value: userToken['refresh_token']['token'],
+    );
+    await storage.write(
+      key: 'userRefreshTokenExpiredAt',
+      value: userToken['refersh_token']['expired_at'],
     );
   }
 
+  // Get Admin
   Future<String?> getAdminAccessToken() =>
       storage.read(key: 'adminAccessToken');
+
+  Future<String?> getAdminAccessTokenExpiredAt() =>
+      storage.read(key: 'adminAccessTokenExpiredAt');
 
   Future<String?> getAdminRefreshToken() =>
       storage.read(key: 'adminRefreshToken');
 
-  Future<String?> getDeviceAccessToken() => storage.read(key: 'deviceAccessToken');
+  Future<String?> getAdminRefreshTokenExpiredAt() =>
+      storage.read(key: 'adminRefreshTokenExpiredAt');
 
-  Future<String?> getDeviceRefreshToken() => storage.read(key: 'deviceAccessToken');
+  // Get Device
+  Future<String?> getDeviceAccessToken() =>
+      storage.read(key: 'deviceAccessToken');
 
-  Future<String?> getUserAccessToken() => storage.read(key: 'userAcessToken');
+  Future<String?> getDeviceAccessTokenExpiredAt() =>
+      storage.read(key: 'deviceAccessTokenExpiredAt');
 
-  Future<String?> getUserRefreshToken() => storage.read(key: 'userRefreshToken');
+  Future<String?> getDeviceRefreshToken() =>
+      storage.read(key: 'deviceRefreshToken');
+
+  Future<String?> getDeviceRefreshTokenExpiredAt() =>
+      storage.read(key: 'deviceRefreshTokenExpiredAt');
+
+  // Get User
+  Future<String?> getUserAccessToken() => storage.read(key: 'userAccessToken');
+
+  Future<String?> getUserAccessTokenExpiredAt() =>
+      storage.read(key: 'userAccessTokenExpiredAt');
+
+  Future<String?> getUserRefreshToken() =>
+      storage.read(key: 'userRefreshToken');
+
+  Future<String?> getUserRefreshTokenExpiredAt() =>
+      storage.read(key: 'userRefreshTokenExpiredAt');
 }
--- lib/data/data_sources/auth/user_auth_data_source.dart
+++ lib/data/data_sources/auth/user_auth_data_source.dart
@@ -1,14 +1,13 @@
 import 'package:danuri_flutter/core/storage/token_storage.dart';
 import 'package:danuri_flutter/data/data_sources/data_source.dart';
-import 'package:danuri_flutter/data/models/auth/admin_auth/response/tokens_response.dart';
+import 'package:danuri_flutter/data/models/auth/token/response/tokens_response.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/auth_code_login_request.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/user_login_request.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/sign_up_request.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/response/send_auth_code_response.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/response/user_info_response.dart';
 
-class UserAuthDataSource extends DataSource{
-
+class UserAuthDataSource extends DataSource {
   Future<SendAuthCodeResponse> login(UserLoginRequest request) async {
     final response = await dio.post(
       '$baseUrl/auth/user/phone',
@@ -30,10 +29,7 @@ class UserAuthDataSource extends DataSource{
       '$baseUrl/auth/user/verify',
       data: request.toJson(),
     );
-    Future.wait([
-      TokenStorage().setUserAccessToken(response.data['access_token']['token']),
-      TokenStorage().setUserRefreshToken(response.data['refresh_token']['token'])
-    ]);
+    await TokenStorage().setUserToken(response.data);
     return TokensResponse.fromJson(response.data);
   }
 }
