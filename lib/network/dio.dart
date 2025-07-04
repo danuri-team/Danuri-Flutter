0884eb99bf888727340f04ede7bc9c2ad8969fd4
jonghwan <s23061@gsm.hs.kr>
2025-07-04 10:17:16 +0900

    feat :: 현재시간과 만료시간을 대조하여 토큰 갱신 로직 구현

--- lib/network/dio.dart
+++ lib/network/dio.dart
@@ -1,3 +1,7 @@
+import 'package:danuri_flutter/core/storage/token_storage.dart';
+import 'package:danuri_flutter/data/data_sources/auth/admin_auth_data_source.dart';
+import 'package:danuri_flutter/data/data_sources/auth/device_auth_data_source.dart';
+import 'package:danuri_flutter/data/models/auth/token/request/refresh_token_request.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -27,14 +31,42 @@ class _AppDio with DioMixin implements AppDio {
     interceptors.addAll(
       [
         InterceptorsWrapper(
-          // onError: (error, handler) async{
-          //   if(error.response?.statusCode == 403){
-          //     final adminRefreshToken = await TokenStorage().getAdminRefreshToken();
-          //     final TokensResponse tokens = await AdminAuthDataSource().refreshToken(RefreshTokenRequest(refresh_token: adminRefreshToken!));
-          //     TokenStorage().setAdminAccessToken(tokens['access_token']['token']);
-          //   }
-          // },
+          onError: (error, handler) async {
+            //토큰이 만료되었을 때 DioException발생
+            if (error.response?.statusCode == 403) {
+              int adminAccessTokenExpiredAt;
+              int deviceAccessTokenExpiredAt;
+              int nowTime = DateTime.now().millisecondsSinceEpoch;
 
+              //토큰들의 만료시간 가져오기
+              Future.wait([
+                TokenStorage().getAdminAccessTokenExpiredAt(),
+                TokenStorage().getDeviceAccessTokenExpiredAt(),
+                TokenStorage().getUserAccessTokenExpiredAt(),
+              ]).then(
+                (value) async {
+                  adminAccessTokenExpiredAt = int.parse(value[0]!);
+                  deviceAccessTokenExpiredAt = int.parse(value[1]!);
+                  final TokenStorage storage = TokenStorage();
+
+                  //현재시간과 만료시간 대조
+                  if (nowTime > adminAccessTokenExpiredAt) {
+                    await storage.getAdminRefreshToken().then(
+                          (refreshToken) => AdminAuthDataSource().refreshToken(
+                            RefreshTokenRequest(refreshToken: refreshToken!),
+                          ),
+                        );
+                  } else if (nowTime > deviceAccessTokenExpiredAt) {
+                    await storage.getDeviceRefreshToken().then(
+                          (refreshToken) => DeviceAuthDataSource().refreshToken(
+                            RefreshTokenRequest(refreshToken: refreshToken!),
+                          ),
+                        );
+                  }
+                },
+              );
+            }
+          },
           onRequest: (options, handler) async {
             return handler.next(options);
           },
