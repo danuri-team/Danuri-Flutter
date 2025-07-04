1f3974da5720e3e566bcd5b020672952eec005b6
jonghwan <s23061@gsm.hs.kr>
2025-07-04 13:43:14 +0900

    Revert "Merge pull request #37 from danuri-team/revert-36-feature/#19-implement_token_mgmt_logic"
    This reverts commit e5359328eaf64c401dff2033429341e1335a0cf7, reversing
    changes made to 6b640c78ce7e1510e35492bf91a98058b811a48e.

--- lib/data/models/auth/token/response/token_response.g.dart
+++ lib/data/models/auth/token/response/token_response.g.dart
@@ -1,19 +1,18 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'access_token_response.dart';
+part of 'token_response.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-AccessTokenResponse _$AccessTokenResponseFromJson(Map<String, dynamic> json) =>
-    AccessTokenResponse(
+TokenResponse _$TokenResponseFromJson(Map<String, dynamic> json) =>
+    TokenResponse(
       token: json['token'] as String,
       expiredAt: (json['expired_at'] as num).toInt(),
     );
 
-Map<String, dynamic> _$AccessTokenResponseToJson(
-        AccessTokenResponse instance) =>
+Map<String, dynamic> _$TokenResponseToJson(TokenResponse instance) =>
     <String, dynamic>{
       'token': instance.token,
       'expired_at': instance.expiredAt,
