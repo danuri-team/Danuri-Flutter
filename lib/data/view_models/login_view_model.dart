9cc22efa384d7014b86bc3329299761b359f0ee5
jonghwan <s23061@gsm.hs.kr>
2025-07-28 17:43:51 +0900

    remove :: 유저 로그인 반환값 void처리

--- lib/data/view_models/login_view_model.dart
+++ lib/data/view_models/login_view_model.dart
@@ -1,20 +1,16 @@
 import 'package:danuri_flutter/data/data_sources/auth/user_auth_data_source.dart';
 import 'package:danuri_flutter/data/models/auth/user_auth/request/user_login_request.dart';
-import 'package:danuri_flutter/data/models/auth/user_auth/response/send_auth_code_response.dart';
 import 'package:dio/dio.dart';
 
 class LoginViewModel {
   final UserAuthDataSource _dataSource = UserAuthDataSource();
-
-  SendAuthCodeResponse? _userInfo;
-  SendAuthCodeResponse? get userInfo => _userInfo;
 
   bool? _error;
   bool? get error => _error;
 
   Future<void> userLogin(String phoneNumber) async {
     try {
-      _userInfo = await _dataSource.login(
+      await _dataSource.login(
         UserLoginRequest(phone: phoneNumber),
       );
       _error = false;
