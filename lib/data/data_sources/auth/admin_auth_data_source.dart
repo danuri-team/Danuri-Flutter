bb6b872a22bb962f12c7d4a1dfe88e43274abfbc
jonghwan <s23061@gsm.hs.kr>
2025-08-20 16:11:50 +0900

    feat :: 관리자, 도움 요청 api추가

--- lib/data/data_sources/auth/admin_auth_data_source.dart
+++ lib/data/data_sources/auth/admin_auth_data_source.dart
@@ -1,11 +1,13 @@
 import 'package:danuri_flutter/core/storage/token_storage.dart';
 import 'package:danuri_flutter/data/data_sources/data_source.dart';
+import 'package:danuri_flutter/data/models/auth/common/request/auth_code_login_request.dart';
+import 'package:danuri_flutter/data/models/auth/common/request/phone_request.dart';
 import 'package:danuri_flutter/data/models/auth/common/response/tokens_response.dart';
 import 'package:danuri_flutter/data/models/auth/admin_auth/request/admin_login_request.dart';
 import 'package:danuri_flutter/data/models/auth/admin_auth/request/sign_up_request.dart';
 
 class AdminAuthDataSource extends DataSource {
-  Future<TokensResponse> login(AdminLoginRequest request) async {
+  Future<TokensResponse> adminLogin(AdminLoginRequest request) async {
     final response = await dio.post(
       '$baseUrl/auth/admin/sign-in',
       data: request.toJson(),
@@ -19,5 +21,20 @@ class AdminAuthDataSource extends DataSource {
       '$baseUrl/auth/admin/sign-up',
       data: request.toJson(),
     );
+  }
+
+  Future<void> findPassword(PhoneRequest request) async {
+    await dio.post(
+      '$baseUrl/auth/admin/find-password',
+      data: request.toJson(),
+    );
+  }
+
+  Future<TokensResponse> resetToken(AuthCodeLoginRequest request) async {
+    final response = await dio.post(
+      '$baseUrl/auth/admin/reset-token',
+      data: request.toJson(),
+    );
+    return TokensResponse.fromJson(response.data);
   }
 }
