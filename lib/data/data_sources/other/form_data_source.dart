5e3f273d35db1b724fd7661c98a941da78f4473e
jonghwan <s23061@gsm.hs.kr>
2025-09-01 21:13:31 +0900

    feat :: 폼 데이터소스 구현

---
+++ lib/data/data_sources/other/form_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:danuri_flutter/data/data_sources/data_source.dart';
+import 'package:danuri_flutter/data/models/other/form/request/form_request.dart';
+import 'package:danuri_flutter/data/models/other/form/response/form_response.dart';
+import 'package:dio/dio.dart';
+
+class FormDataSource extends DataSource{
+  Future<FormResponse> getForm() async {
+    final response = await dio.get(
+      '/form',
+      options: Options(headers: {'Authorization': 'Bearer ${await deviceToken}'}),
+    );
+    return FormResponse.fromJson(response.data);
+  }
+
+  Future<void> getUsageSpace(FormRequest request) async {
+    await dio.post(
+      '/form',
+      data: request.toJson(),
+      options: Options(headers: {'Authorization': 'Bearer ${await userToken}'}),
+    );
+  }
+}
