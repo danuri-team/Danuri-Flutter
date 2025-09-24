e89408fcda413d953c46450721e84abf36c08fdb
jonghwan <s23061@gsm.hs.kr>
2025-09-24 14:05:30 +0900

    feat :: sentry 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,46 @@
 import 'package:danuri_flutter/config/app_routes.dart';
 import 'package:danuri_flutter/core/theme/color.dart';
 import 'package:danuri_flutter/config/router.dart';
-import 'package:danuri_flutter/core/storage/token_storage.dart';
 import 'package:danuri_flutter/data/view_models/admin_auth_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  bool firstRun = false;
 
-  // if (adminToken == null) {
   final viewModel = AdminAuthViewModel();
   await viewModel.adminLogin();
-  // }
 
-  // if (deviceToken == null) firstRun = true;
+  await SentryFlutter.init(
+    
+    (options) {
+      options.dsn =
+          'https://9b8dc65a4e46ca2f692708c053b096a4@o4508238543060992.ingest.us.sentry.io/4510067909787648';
 
-  runApp(MyApp(firstRun: firstRun));
+      options.sendDefaultPii = true;
+
+      options.tracesSampleRate = 1.0;
+      options.profilesSampleRate = 1.0;
+
+      options.replay.onErrorSampleRate = 1.0;
+      options.replay.sessionSampleRate = 1.0;
+    },
+    appRunner: () => runApp(
+      SentryWidget(
+        child: MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-    required this.firstRun,
-  });
-
-  final bool firstRun;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -45,11 +54,7 @@ class MyApp extends StatelessWidget {
               scaffoldBackgroundColor: DanuriColor.background1,
               fontFamily: 'Pretendard',
             ),
-            routerConfig: router(
-              // firstRun ? AppRoutes.organAuth : AppRoutes.home,
-              AppRoutes.organAuth
-              // AppRoutes.home,
-            ),
+            routerConfig: router(AppRoutes.organAuth),
           );
         },
       ),
