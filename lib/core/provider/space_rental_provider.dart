8745fcf2df4ee4e6eea14e56f8c7d68bae796e1d
jonghwan <s23061@gsm.hs.kr>
2025-10-15 10:53:11 +0900

    feat :: 공간 대여 시작 시간 상태관리 추가

--- lib/core/provider/space_rental_provider.dart
+++ lib/core/provider/space_rental_provider.dart
@@ -3,3 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final spaceIdProvider = StateProvider<String?>(
   (ref) => null,
 );
+
+final startAtProvider = StateProvider<String?>(
+  (ref) => null,
+);
