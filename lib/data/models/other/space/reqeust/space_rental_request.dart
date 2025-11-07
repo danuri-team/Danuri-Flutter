3bb71ba299ecd1a7a15816142ced6d2307de4eb1
jonghwan <s23061@gsm.hs.kr>
2025-11-07 11:32:40 +0900

    chore :: 공간 등록 API, 추가 인원 필드 추가

--- lib/data/models/other/space/reqeust/space_rental_request.dart
+++ lib/data/models/other/space/reqeust/space_rental_request.dart
@@ -1,3 +1,5 @@
+import 'package:danuri_flutter/core/enum/age_type.dart';
+import 'package:danuri_flutter/core/enum/gender_type.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'space_rental_request.g.dart';
@@ -8,14 +10,36 @@ class SpaceRentalRequest {
   final String spaceId;
   @JsonKey(name: 'start_At')
   final String startAt;
+  @JsonKey(name: 'additional_participants')
+  final List<AdditionalParticipants> additionalParticipants;
 
   SpaceRentalRequest({
     required this.spaceId,
     required this.startAt,
+    required this.additionalParticipants,
   });
 
   factory SpaceRentalRequest.fromJson(Map<String, dynamic> json) =>
       _$SpaceRentalRequestFromJson(json);
 
   Map<String, dynamic> toJson() => _$SpaceRentalRequestToJson(this);
+}
+
+@JsonSerializable()
+class AdditionalParticipants {
+  final GenderType sex;
+  @JsonValue('age_group')
+  final AgeType ageGroup;
+  final int count;
+
+  const AdditionalParticipants({
+    required this.sex,
+    required this.ageGroup,
+    required this.count,
+  });
+
+  factory AdditionalParticipants.fromJson(Map<String, dynamic> json) =>
+      _$AdditionalParticipantsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AdditionalParticipantsToJson(this);
 }
--- lib/data/models/other/space/reqeust/space_rental_request.g.dart
+++ lib/data/models/other/space/reqeust/space_rental_request.g.dart
@@ -10,10 +10,44 @@ SpaceRentalRequest _$SpaceRentalRequestFromJson(Map<String, dynamic> json) =>
     SpaceRentalRequest(
       spaceId: json['space_id'] as String,
       startAt: json['start_At'] as String,
+      additionalParticipants: (json['additional_participants'] as List<dynamic>)
+          .map(
+              (e) => AdditionalParticipants.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$SpaceRentalRequestToJson(SpaceRentalRequest instance) =>
     <String, dynamic>{
       'space_id': instance.spaceId,
       'start_At': instance.startAt,
+      'additional_participants': instance.additionalParticipants,
     };
+
+AdditionalParticipants _$AdditionalParticipantsFromJson(
+        Map<String, dynamic> json) =>
+    AdditionalParticipants(
+      sex: $enumDecode(_$GenderTypeEnumMap, json['sex']),
+      ageGroup: $enumDecode(_$AgeTypeEnumMap, json['ageGroup']),
+      count: (json['count'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$AdditionalParticipantsToJson(
+        AdditionalParticipants instance) =>
+    <String, dynamic>{
+      'sex': _$GenderTypeEnumMap[instance.sex]!,
+      'ageGroup': _$AgeTypeEnumMap[instance.ageGroup]!,
+      'count': instance.count,
+    };
+
+const _$GenderTypeEnumMap = {
+  GenderType.MALE: 'MALE',
+  GenderType.FEMALE: 'FEMALE',
+};
+
+const _$AgeTypeEnumMap = {
+  AgeType.ELEMENTARY: 'ELEMENTARY',
+  AgeType.MIDDLE: 'MIDDLE',
+  AgeType.HIGH: 'HIGH',
+  AgeType.OUT_OF_SCHOOL_YOUTH: 'OUT_OF_SCHOOL_YOUTH',
+  AgeType.ADULT: 'ADULT',
+};
